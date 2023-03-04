b00d6e5586cbfead2ed6c87b963a22f8fe20c368
benno <BennoCrafterDev@googlemail.com>
2023-03-04 16:28:31 +0100

    added deltatime

--- testing.py
+++ testing.py
@@ -1,12 +1,12 @@
 from ve.ve import GameEngine, GameObject
 
 # Initialize the game engine
-game = GameEngine(800, 600, "Demo Game")
+game = GameEngine(screen_width=800, screen_height=600, screen_name="Demo Game", screen_color="yellow")
 
 # Create game objects
-player = GameObject(100, 100, 50, 50, 2)
-enemy = GameObject(500, 300, 50, 50, 3)
-test = GameObject(200, 200, 60, 60, 4)
+player = GameObject(x=100, y=100, width=50, height=50, color="blue")
+enemy = GameObject(x=500, y=300, width=50, height=50, color="red")
+test = GameObject(200, 200, 60, 60, "pink")
 
 # Add game objects to the game engine
 game.add_object(player)
@@ -15,14 +15,21 @@ game.add_object(test)
 # move game object
 test.move(5, 0)
 # delete game object
-game.delete_object(player)
+# game.delete_object(enemy)
 
 
-def update():
+def update(deltatime):
     if game.key_pressed("d"):
-        enemy.move(1)
+        player.move(dx=player.speed * deltatime, dy=0)
     elif game.key_pressed("a"):
-        enemy.move(-1)
+        player.move(dx=-player.speed * deltatime)
+    elif game.key_pressed("s"):
+        player.move(0, player.speed * deltatime)
+    elif game.key_pressed("w"):
+        player.move(0, -player.speed + deltatime)
+
+    if player.collided(enemy):
+        print("yey")
 
 
 game.update = update
--- ve/ve.py
+++ ve/ve.py
@@ -1,16 +1,29 @@
 import sdl2.ext
-# Load the script as a module
+import time
+
+color_dict = {
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "orange": (255, 165, 0),
+    "purple": (128, 0, 128),
+    "pink": (255, 192, 203),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255)
+}
 
 
 class GameEngine:
-    def __init__(self, screen_width, screen_height, name):
+    def __init__(self, screen_width=None, screen_height=None, screen_name="Default", screen_color=None):
         sdl2.ext.init()
         self.objects = []
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.current_key = int
-        self.window = sdl2.ext.Window(name, size=(screen_width, screen_height))
-        self.window.show()
+        if screen_color in color_dict:
+            self.screen_color = color_dict[screen_color]
+        self.current_key = None
+        self.window = sdl2.ext.Window(screen_name, size=(screen_width, screen_height))
 
     def add_object(self, obj):
         self.objects.append(obj)
@@ -23,29 +36,30 @@ class GameEngine:
             return True
 
     def render(self):
-        sdl2.SDL_SetRenderDrawColor(self.renderer.sdlrenderer, 255, 255, 255, 255)
+        # background
+        sdl2.SDL_SetRenderDrawColor(self.renderer.sdlrenderer, *self.screen_color, 255)
         sdl2.SDL_RenderClear(self.renderer.sdlrenderer)
-        sdl2.SDL_SetRenderDrawColor(self.renderer.sdlrenderer, 0, 0, 255, 255)
-
         for obj in self.objects:
+            # objects
+            sdl2.SDL_SetRenderDrawColor(self.renderer.sdlrenderer, *obj.color, 255)
             rect = sdl2.rect.SDL_Rect(int(obj.x), int(obj.y), int(obj.width), int(obj.height))
             sdl2.SDL_RenderFillRect(self.renderer.sdlrenderer, rect)
 
-        sdl2.SDL_RenderPresent(self.renderer.sdlrenderer)
+        self.renderer.present()
 
     def close_window(self):
         sdl2.ext.quit()
 
     def start(self):
         # Create the renderer
-        self.renderer = sdl2.ext.Renderer(self.window)
-
+        self.renderer = sdl2.ext.Renderer(self.window, flags=sdl2.SDL_RENDERER_ACCELERATED)
+        self.window.show()
         # Start the game loop
         self.game_loop()
 
     def game_loop(self):
         # Get the current time
-
+        last_time = time.monotonic()
         running = True
         while running:
             events = sdl2.ext.get_events()  # Get events from queue
@@ -57,7 +71,10 @@ class GameEngine:
                     self.current_key = self.key_to_text(event.key.keysym.sym)
                 elif event.type == sdl2.SDL_KEYUP:
                     self.current_key = None
-            self.update()
+            current_time = time.monotonic()
+            delta_time = current_time - last_time
+            last_time = current_time
+            self.update(delta_time=delta_time)
             self.render()
         self.window.close()
         sdl2.ext.quit()
@@ -75,12 +92,21 @@ class GameEngine:
 
 class GameObject:
     def __init__(self, x, y, width, height, color):
-        self.x = int(x)
-        self.y = int(y)
+        self.x = x
+        self.y = y
         self.width = int(width)
         self.height = int(height)
-        self.color = color
+        self.speed = 100  # pixel per second
+        if color in color_dict:
+            self.color = color_dict[color]
 
     def move(self, dx=0, dy=0):
         self.x += dx
         self.y += dy
+
+    def collided(self, other):
+        # Check if the two game objects intersect
+        return (self.x < other.x + other.width and
+                self.x + self.width > other.x and
+                self.y < other.y + other.height and
+                self.y + self.height > other.y)
